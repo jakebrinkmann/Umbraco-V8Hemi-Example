395ddcd26b91af62912bc5bcddffd79c36d9d680
Joe Kepley <joe@blendinteractive.com>
2024-09-27 10:03:51 -0500

    Added basic dashboard UI

---
+++ Blend.V8Hemi.Frontend/assets/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: "src/my-element.ts", // your web component source file
+            formats: ["es"],
+        },
+        outDir: "../wwwroot/app_plugins/blend.v8hemi.frontend",
+        emptyOutDir: true,
+        sourcemap: true,
+        rollupOptions: {
+            external: [/^@umbraco/],
+            onwarn: () => { },
+        },
+    },
+});
